58e21500d20b9379999c713d300e2d8a698c485e
pengzhenghao <pzh@cs.ucla.edu>
2024-01-29 12:50:46 -0800

    Fix the dependencies issues

--- copo_code/setup.py
+++ copo_code/setup.py
@@ -8,8 +8,8 @@ setup(
     name="copo",
     install_requires=[
         "yapf==0.30.0",
-        "ray==1.2.0",
-        "ray[all]==1.2.0",
+        "ray==2.2.0",
+        "ray[rllib]==2.2.0",
         "tensorflow==2.3.1",
         "torch",
         "tensorflow-probability==0.11.1",
